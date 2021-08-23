6da2f1f6208ca3a778791e6231eae4992a88bcc3
Manuel Mehltretter <mehltretter.manuel@gmail.com>
2021-08-23 14:18:00 +0200

    Create noip6updater.sh

---
+++ noip6updater.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Author: Manuel Mehltretter
+# NoIPv6Updater
+# Updates specified hostnames with the machines global IPv6
+# NoIP API Documentation: https://www.noip.com/integrate/request
+
+### Script configuration ###
+
+USER='joe.average@gmail.com'
+PASSWORD='yourpass'
+INTERFACE='eth0'
+HOSTNAMES=('your.domain.io')
+LOGFILE='/var/log/noip6updater.log'
+
+### No changes below this line ###
+
+# Get global IPv6 address from specified interface
+ip=$(ip -6 addr show dev $INTERFACE scope global | grep inet6 | awk -F '[ \t]+|/' '{print $3}')
+
+# Update every hostname in the array
+for hostname in $HOSTNAMES
+do
+    # NoIP API call
+    result=$(curl -s -u "$USER:$PASSWORD" "https://dynupdate.no-ip.com/nic/update?hostname=$hostname&myip=$ip")
+    # Log results: nochg = Provided IP was already set; good = IP was updated, both are a success.
+    echo $(date -u) ": API call respone: $result" >> $LOGFILE
+done
