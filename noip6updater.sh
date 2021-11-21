ce8077261d90938d0706b2527ab59596c78dcdc7
Manuel Mehltretter <mehltretter.manuel@gmail.com>
2021-11-21 17:21:14 +0100

    Update noip6updater.sh
    Fix wrong syntax resulting in only the first hostname to be updated.

--- noip6updater.sh
+++ noip6updater.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Author: Manuel Mehltretter
 # NoIPv6Updater
-# Updates specified hostnames with the machines global IPv6
+# Updates specified hostnames with the provided interfaces global IPv6
 # NoIP API Documentation: https://www.noip.com/integrate/request
 
 ### Script configuration ###
@@ -18,7 +18,7 @@ LOGFILE='/var/log/noip6updater.log'
 ip=$(ip -6 addr show dev $INTERFACE scope global | grep inet6 | awk -F '[ \t]+|/' '{print $3}')
 
 # Update every hostname in the array
-for hostname in $HOSTNAMES
+for hostname in ${HOSTNAMES[@]}
 do
     # NoIP API call
     result=$(curl -s -u "$USER:$PASSWORD" "https://dynupdate.no-ip.com/nic/update?hostname=$hostname&myip=$ip")
